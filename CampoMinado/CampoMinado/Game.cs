68c80a0f0b0d435b5248048eb5b0575b7b61d7b1
Hamilton Monteiro <hamiltonmneto@gmail.com>
2018-04-18 23:50:15 -0300

    Some new implementations

--- CampoMinado/CampoMinado/Game.cs
+++ CampoMinado/CampoMinado/Game.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 
 namespace CampoMinado
 {
-    class Game
+    public class Game
     {
         private int _largura;
         private int _altura;
         private int _minas;
         private Panel _panel;
+        private Sqm[,] _Sqms;
 
         public Game(Panel panel, int largura, int altura, int minas)
         {
@@ -25,10 +22,71 @@ namespace CampoMinado
 
         public void start()
         {
-            
+            Panel.Controls.Clear();
+            _Sqms = new Sqm[largura, altura];
+            for (int x = 0; x < largura; x++)
+            {
+                for (int y = 0; y < largura; y++)
+                {
+                    Sqm s = new Sqm(this, x, y);
+                    s.Explode += new EventHandler(Explode);
+                    _Sqms[x, y] = s;
+                }
+            }
+
+            int b = 0;
+            Random r = new Random();
+            while (b < Minas)
+            {
+                int x = r.Next(altura);
+                int y = r.Next(largura);
+
+                Sqm s = _Sqms[x, y];
+                if (!s.Minado)
+                {
+                    s.Minado = true;
+                    b++;
+                }
+            }
+        }
+
+        private void Explode(object sender, EventArgs e)
+        {
+
+            foreach (Sqm s in _Sqms)
+            {
+                s.RemoveEvents();
+                if (s.Minado)
+                {
+                    s.Button.Text = "*";
+                    s.Button.Font = new System.Drawing.Font("Arial Black", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+                    s.Button.ForeColor = Color.Black;
+                }
+
+            }
         }
 
         public EventHandler Tick { get; internal set; }
         public object Time { get; internal set; }
+
+        public Panel Panel
+        {
+            get { return (this._panel); }
+        }
+
+        public int largura
+        {
+            get { return (this._largura); }
+        }
+
+        public int altura
+        {
+            get { return (this._altura); }
+        }
+
+        public int Minas
+        {
+            get { return (this._minas); }
+        }
     }
 }
---
+++ CampoMinado/CampoMinado/Sqm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CampoMinado
+{
+    public class Sqm
+    {
+        public event EventHandler Explode;
+
+        private Button _button;
+        private bool _desmontado = false;
+        private Game _game;
+        private bool _minado = false;
+        private bool _aberto = false;
+        private int _x;
+        private int _y;
+        private bool _gameOver = false;
+
+        public Sqm(Game game, int x, int y)
+        {
+            _game = game;
+            _x = x;
+            _y = y;
+            _button = new Button();
+            Button.Text = "";
+
+            int w = _game.Panel.Width / _game.largura;
+            int h = _game.Panel.Height / _game.altura;
+
+            _button.Width = w + 1;
+            _button.Height = h + 1;
+            _button.Left = w * X;
+            _button.Top = h * Y;
+            _button.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+            _button.Click += new EventHandler(Click);
+
+            _game.Panel.Controls.Add(Button);
+        }
+
+        private void Click(object sender, System.EventArgs e)
+        {
+            if (!Desmontado)
+            {
+                if (Minado)
+                {
+                    Button.BackColor = Color.Red;
+                    _gameOver = false;
+
+                }
+            }
+        }
+
+        protected void OnExplode()
+        {
+            if (Explode != null)
+            {
+                Explode(this, new EventArgs());
+            }
+        }
+
+        public void RemoveEvents()
+        {
+            _button.Click -= new EventHandler(Click);
+        }
+
+        public Button Button
+        {
+            get { return (this._button); }
+        }
+
+        public int X
+        {
+            get { return (this._x); }
+        }
+
+        public int Y
+        {
+            get { return (this._y); }
+        }
+
+        public bool Desmontado
+        {
+            get { return (this._desmontado); }
+        }
+
+        public bool Minado
+        {
+            get { return (this._minado); }
+            set { this._minado = value; }
+        }
+
+        public bool GameOver
+        {
+            get { return (this._gameOver); }
+            set { this._gameOver = value; }
+        }
+    }
+}
